0174b22658752652dc6a836f1969fd08b4757fab
Tal Zahavi <tal.zahavi9@gmail.com>
2015-12-31 00:19:26 +0200

    Change graph representation + add some code

--- DpTrainer.py
+++ DpTrainer.py
@@ -23,19 +23,20 @@ class DpTrainer:
         self.feature_10_dict = dict()
         self.feature_13_dict = dict()
 
-        # TODO: Need this???
         # TODO: Ask Omer - There's sentences in the training that appear more than once!
-        self.sentence_to_arch_dict = dict()
+        self.arches_data = []
 
         self.features = dict()
         self.feature_num = 0
 
-        self.calculated_trees_score = dict()
+        self.graphs = []  # Holds (graph, full_graph)
+        self.scored_graphs = []  # Holds (graph, graph score,  full_graph, full_graph score)
 
     #################
     # FEATURES PART #
     #################
 
+    # Add all seen features to the dicts
     def get_features(self):
         sentence_words_pos = dict()
         with open('Data\\train.labeled', 'r') as f:
@@ -52,22 +53,13 @@ class DpTrainer:
                                                sentence_words_pos[word_tuple[2]][1], word_tuple[0], word_tuple[1])
                         self.add_features_to_dicts(dependency_arch)
                         arches.add(dependency_arch)
-                    self.sentence_to_arch_dict[self.get_sentence(sentence_words_pos)] = arches
+                    self.arches_data.append(arches)
                     sentence_words_pos = dict()
                 else:
                     split_line = line.split('\t')
                     # (counter)->(token,pos,head)
                     sentence_words_pos[int(split_line[0])] = (split_line[1], split_line[3], int(split_line[6]))
         f.close()
-
-    # Get labeled data, and return only the sentence
-    @staticmethod
-    def get_sentence(data_tuple):
-        sentence = ''
-        for i in range(1, len(data_tuple)):
-            sentence += data_tuple[i][0] + ' '
-        sentence += data_tuple[len(data_tuple)][0]
-        return sentence
 
     # Get a dependency arch, and add all the possible features to the dict
     def add_features_to_dicts(self, dependency_arch):
@@ -91,21 +83,37 @@ class DpTrainer:
         else:
             feature_dict[feature_data] = 1
 
+    # Create a graph representation of the sentence (for edmond)
+    # TODO: NEED TO MAKE ALSO FULL GRAPH HERE! IMPLEMENT full_g!
+    def make_graphs(self):
+        for arches_data in self.arches_data:
+            g = dict()
+            full_g = dict()
+            for arch in arches_data:
+                if (arch[0], arch[1]) in g:
+                    g[(arch[0], arch[1])][(arch[2], arch[3])] = 0
+                else:
+                    g[(arch[0], arch[1])] = {(arch[2], arch[3]): 0}
+
+            self.graphs.append((g, full_g))
+
     ##########################
     # FREQUENT FEATURES PART #
     ##########################
 
+    # Get only frequent features (configurable limits)
     def get_frequent_features(self):
         self.add_frequent_feature(self.FEATURE_1_LIMIT, self.feature_1_dict, 1)
         self.add_frequent_feature(self.FEATURE_2_LIMIT, self.feature_2_dict, 2)
         self.add_frequent_feature(self.FEATURE_3_LIMIT, self.feature_3_dict, 3)
-        self.add_frequent_feature(self.FEATURE_4_LIMIT, self.feature_4_dict,4)
+        self.add_frequent_feature(self.FEATURE_4_LIMIT, self.feature_4_dict, 4)
         self.add_frequent_feature(self.FEATURE_5_LIMIT, self.feature_5_dict, 5)
         self.add_frequent_feature(self.FEATURE_6_LIMIT, self.feature_6_dict, 6)
         self.add_frequent_feature(self.FEATURE_8_LIMIT, self.feature_8_dict, 8)
         self.add_frequent_feature(self.FEATURE_10_LIMIT, self.feature_10_dict, 10)
         self.add_frequent_feature(self.FEATURE_13_LIMIT, self.feature_13_dict, 13)
 
+    # For a specific feature dict
     def add_frequent_feature(self, limit, feature_dict, dict_num):
         counter = 0
         for key in feature_dict:
@@ -120,35 +128,55 @@ class DpTrainer:
     # Pre Calculate #
     #################
 
-    def calculate_real_score(self):
-        for sentence in self.sentence_to_arch_dict:
-            num_features = []
-            for dependency_arch in self.sentence_to_arch_dict[sentence]:
-                if ((dependency_arch[0], dependency_arch[1]), 1) in self.features:
-                    num_features.append(self.features[((dependency_arch[0], dependency_arch[1]), 1)])
-                if ((dependency_arch[0], ''), 2) in self.features:
-                    num_features.append(self.features[((dependency_arch[0], ''), 2)])
-                if ((dependency_arch[1], ''), 3) in self.features:
-                    num_features.append(self.features[((dependency_arch[1], ''), 3)])
-                if ((dependency_arch[2], dependency_arch[3]), 4) in self.features:
-                    num_features.append(self.features[((dependency_arch[2], dependency_arch[3]), 4)])
-                if ((dependency_arch[2], ''), 5) in self.features:
-                    num_features.append(self.features[((dependency_arch[2], ''), 5)])
-                if ((dependency_arch[3], ''), 6) in self.features:
-                    num_features.append(self.features[((dependency_arch[3], ''), 6)])
-                if ((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8) in self.features:
-                    num_features.append(self.features[((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8)])
-                if (((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10) in self.features:
-                    num_features.append(self.features[(((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10)])
-                if ((dependency_arch[1], dependency_arch[3]), 13) in self.features:
-                    num_features.append(self.features[((dependency_arch[1], dependency_arch[3]), 13)])
+    # Return the features number that return 1 for a given arch
+    def get_features_for_arch(self, dependency_arch):
+        num_features = []
+        if ((dependency_arch[0], dependency_arch[1]), 1) in self.features:
+            num_features.append(self.features[((dependency_arch[0], dependency_arch[1]), 1)])
+        if ((dependency_arch[0], ''), 2) in self.features:
+            num_features.append(self.features[((dependency_arch[0], ''), 2)])
+        if ((dependency_arch[1], ''), 3) in self.features:
+            num_features.append(self.features[((dependency_arch[1], ''), 3)])
+        if ((dependency_arch[2], dependency_arch[3]), 4) in self.features:
+            num_features.append(self.features[((dependency_arch[2], dependency_arch[3]), 4)])
+        if ((dependency_arch[2], ''), 5) in self.features:
+            num_features.append(self.features[((dependency_arch[2], ''), 5)])
+        if ((dependency_arch[3], ''), 6) in self.features:
+            num_features.append(self.features[((dependency_arch[3], ''), 6)])
+        if ((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8) in self.features:
+            num_features.append(self.features[((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8)])
+        if (((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10) in self.features:
+            num_features.append(self.features[(((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10)])
+        if ((dependency_arch[1], dependency_arch[3]), 13) in self.features:
+            num_features.append(self.features[((dependency_arch[1], dependency_arch[3]), 13)])
+        return num_features
 
-                self.calculated_trees_score[sentence] = num_features
+    # Get a graph, and return a list lists
+    # Each list holds the features number that return 1 for that arch
+    def get_features_for_graph(self, g):
+        features_list = []
+        for head_data in g:
+            for child_data in g[head_data]:
+                dependency_arch = (head_data[0], head_data[1], child_data[0], child_data[1])
+                features_list.append(self.get_features_for_arch(dependency_arch))
+        return features_list
+
+    # Doing this calculation one time !
+    # checking the features for each graph (real and full)
+    def calculate_features_for_all_graphs(self):
+        for data in self.graphs:
+            self.scored_graphs.append((data[0], self.get_features_for_graph(data[0]),
+                                       data[1], self.get_features_for_graph(data[1])))
+
+    #########
+    # Train #
+    #########
 
     def train(self):
         start_time = datetime.now()
         print('\nGetting all features...')
         self.get_features()
+        self.make_graphs()
         print('Found the following features:')
         print('------------------------------')
         print(str(len(self.feature_1_dict)) + ' of feature 1')
@@ -170,8 +198,8 @@ class DpTrainer:
         self.get_frequent_features()
         print('***Total of ' + str(len(self.features)) + ' optimized features***')
 
-        print('\nCalculating the score for all labeled sentence...')
-        self.calculate_real_score()
+        print('\nDoing some magic...')
+        self.calculate_features_for_all_graphs()
         print('All done!')
 
         print('\nTHE LEARNING PROCESS TOOK ' + str(datetime.now()-start_time))
