1181bbd0abbc71fee7c6912f40a78ef6ea2f63ac
Tal Zahavi <tal.zahavi9@gmail.com>
2016-01-04 23:54:19 +0200

    Got Out Aux + Refactor Inference

--- Inference.py
+++ Inference.py
@@ -35,7 +35,6 @@ class Inference:
     # Get unlabeled document, inference the dependencies and write to a new document
     def data_inference(self, unlabeled_document_name, output_name, w):
         sentence_words_pos = dict()
-        # words_pos_to_word_num = dict()
         sentence_num = 0
         with open(unlabeled_document_name, 'r') as f1:
             with open(output_name, 'w+') as f2:
@@ -49,27 +48,22 @@ class Inference:
 
                         data_for_full_graph.add(('root', 'root', 0))
 
-                        # TODO: 6. Replace Aux in Trainer
-
                         full_unweighted_g = AuxFunctions.make_full_graph(list(data_for_full_graph))
                         g_features = AuxFunctions.get_features_for_graph(self.features, full_unweighted_g)
                         full_weighted_g = AuxFunctions.get_weighted_graph(full_unweighted_g, g_features, w)
 
                         g_inference = edmonds.mst(('root', 'root', 0), full_weighted_g)
 
-                        # words_pos_to_word_num[('root', 'root')] = 0
                         inference_arches = self.get_arches_in_order(g_inference)
                         self.write_lines(inference_arches, f2)
 
                         sentence_num += 1
                         print('Done sentence number ' + str(sentence_num))
                         sentence_words_pos = dict()
-                        # words_pos_to_word_num = dict()
                     else:
                         split_line = line.split('\t')
                         # (counter)->(token,pos)
                         sentence_words_pos[int(split_line[0])] = (split_line[1], split_line[3])
-                        # words_pos_to_word_num[(split_line[1], split_line[3])] = int(split_line[0])
 
             f2.close()
         f1.close()
@@ -84,8 +78,8 @@ class Inference:
             for line1, line2 in zip(f1, f2):
                 if line2 is '\n':
                     sentence_num += 1
-                    print('The accuracy after the ' + str(sentence_num) + ' sentence is: ' +
-                          str(round((correct/total)*100, 2)) + '%')
+                    # print('The accuracy after the ' + str(sentence_num) + ' sentence is: ' +
+                    #       str(round((correct/total)*100, 2)) + '%')
                 else:
                     gold_split = line1.split('\t')
                     test_split = line2.split('\t')
@@ -94,10 +88,28 @@ class Inference:
                             gold_split[3] == test_split[3]:
                                 if gold_split[6] == test_split[6]:
                                     correct += 1
-
         f1.close()
         f2.close()
+        return round((correct/total)*100, 2)
 
 z = Inference()
-z.data_inference('Data\\test.unlabeled', 'Data\\test.final', z.w_20)
-z.get_accuracy('Data\\test.labeled', 'Data\\test.final')
+print('Starting inference for w_20...')
+z.data_inference('Data\\test.unlabeled', 'Data\\test.mylabel20', z.w_20)
+result_20 = z.get_accuracy('Data\\test.labeled', 'Data\\test.mylabel20')
+
+print('\nStarting inference for w_50...')
+z.data_inference('Data\\test.unlabeled', 'Data\\test.mylabel50', z.w_50)
+result_50 = z.get_accuracy('Data\\test.labeled', 'Data\\test.mylabel50')
+
+print('\nStarting inference for w_80...')
+z.data_inference('Data\\test.unlabeled', 'Data\\test.mylabel80', z.w_80)
+result_80 = z.get_accuracy('Data\\test.labeled', 'Data\\test.mylabel80')
+
+print('\nStarting inference for w_100...')
+z.data_inference('Data\\test.unlabeled', 'Data\\test.mylabel100', z.w_100)
+result_100 = z.get_accuracy('Data\\test.labeled', 'Data\\test.mylabel100')
+
+print('\nThe accuracy for w_20 is ' + str(result_20) + '%')
+print('The accuracy for w_50 is ' + str(result_50) + '%')
+print('The accuracy for w_80 is ' + str(result_80) + '%')
+print('The accuracy for w_100 is ' + str(result_100) + '%')
--- Trainer.py
+++ Trainer.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import edmonds
 import random
+import AuxFunctions
 
 
 class Trainer:
@@ -111,22 +112,7 @@ class Trainer:
                 else:
                     g[(arch_tuple[0][0], arch_tuple[0][1], arch_tuple[1])] = {(arch_tuple[0][2], arch_tuple[0][3], arch_tuple[2]): 0}
 
-            self.graphs.append((g, self.make_full_graph(arches_data[1])))
-
-    @staticmethod
-    # Get a list of (word,pod,word num) and build a valid full graph
-    def make_full_graph(word_pos_num):
-        full_g = dict()
-        for word_pos_num_i in word_pos_num:
-            for word_pos_num_j in word_pos_num:
-                if word_pos_num_i[0] != word_pos_num_j[0] and word_pos_num_j[0] != 'root':
-
-                    if (word_pos_num_i[0], word_pos_num_i[1], word_pos_num_i[2]) in full_g:
-                        (full_g[(word_pos_num_i[0], word_pos_num_i[1], word_pos_num_i[2])])[(word_pos_num_j[0], word_pos_num_j[1], word_pos_num_j[2])] = 0
-                    else:
-                        full_g[(word_pos_num_i[0], word_pos_num_i[1], word_pos_num_i[2])] = \
-                            {(word_pos_num_j[0], word_pos_num_j[1], word_pos_num_j[2]): 0}
-        return full_g
+            self.graphs.append((g, AuxFunctions.make_full_graph(arches_data[1])))
 
     ##########################
     # FREQUENT FEATURES PART #
@@ -159,67 +145,24 @@ class Trainer:
     # Pre Calculate #
     #################
 
-    # Return the features number that return 1 for a given arch
-    def get_features_for_arch(self, dependency_arch):
-        num_features = []
-        if ((dependency_arch[0], dependency_arch[1]), 1) in self.features:
-            num_features.append(self.features[((dependency_arch[0], dependency_arch[1]), 1)])
-        if ((dependency_arch[0], ''), 2) in self.features:
-            num_features.append(self.features[((dependency_arch[0], ''), 2)])
-        if ((dependency_arch[1], ''), 3) in self.features:
-            num_features.append(self.features[((dependency_arch[1], ''), 3)])
-        if ((dependency_arch[2], dependency_arch[3]), 4) in self.features:
-            num_features.append(self.features[((dependency_arch[2], dependency_arch[3]), 4)])
-        if ((dependency_arch[2], ''), 5) in self.features:
-            num_features.append(self.features[((dependency_arch[2], ''), 5)])
-        if ((dependency_arch[3], ''), 6) in self.features:
-            num_features.append(self.features[((dependency_arch[3], ''), 6)])
-        if ((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8) in self.features:
-            num_features.append(self.features[((dependency_arch[1], (dependency_arch[2], dependency_arch[3])), 8)])
-        if (((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10) in self.features:
-            num_features.append(self.features[(((dependency_arch[0], dependency_arch[1]), dependency_arch[3]), 10)])
-        if ((dependency_arch[1], dependency_arch[3]), 13) in self.features:
-            num_features.append(self.features[((dependency_arch[1], dependency_arch[3]), 13)])
-        return num_features
-
-    # Get a graph, and return a list of lists
-    # Each list holds the features number that return 1 for that arch (and the arch data)
-    def get_features_for_graph(self, g):
-        features_list = []
-        for head_data in g:
-            for child_data in g[head_data]:
-                dependency_arch = (head_data[0], head_data[1], child_data[0], child_data[1])
-                features_list.append(((head_data[0], head_data[1], head_data[2]), (child_data[0], child_data[1], child_data[2]),
-                                      self.get_features_for_arch(dependency_arch)))
-        return features_list
-
     # Doing this calculation one time !
     # checking the features for each graph (real and full)
     def calculate_features_for_all_graphs(self):
         graph_number = 0  # We use this to later save f_vector result in dict
         for data in self.graphs:
             graph_number += 1
-            self.scored_graphs.append((data[0], self.get_features_for_graph(data[0]),
-                                       data[1], self.get_features_for_graph(data[1]), graph_number))
+            self.scored_graphs.append((data[0], AuxFunctions.get_features_for_graph(self.features, data[0]),
+                                       data[1], AuxFunctions.get_features_for_graph(self.features, data[1]),
+                                       graph_number))
 
     ########################
     # Perceptron Algorithm #
     ########################
 
-    @staticmethod
-    # Get a graph, list of features that apply on him and vector w - return the weighted graph
-    def get_weighted_graph(g, features_list, w):
-        for features_edge in features_list:
-            weight = 0
-            for feature_i in features_edge[2]:
-                weight += w[feature_i]
-            g[(features_edge[0][0], features_edge[0][1], features_edge[0][2])][(features_edge[1][0], features_edge[1][1], features_edge[1][2])] = -weight
-        return g
-
     # Return the f vector for a given graph
     def get_f_vector(self, g):
         f_vector = np.zeros(self.feature_num, dtype=int)
-        features = self.get_features_for_graph(g)
+        features = AuxFunctions.get_features_for_graph(self.features, g)
         for feature_data in features:
             for feature_i in feature_data[2]:
                 f_vector[feature_i] += 1
@@ -245,9 +188,9 @@ class Trainer:
             shuffled_scored_graph_index = sorted(scored_graph_index, key=lambda k: random.random())
             for index in shuffled_scored_graph_index:
                 data = self.scored_graphs[index]
-                weighted_full_graph = self.get_weighted_graph(data[2], data[3], w)
+                weighted_full_graph = AuxFunctions.get_weighted_graph(data[2], data[3], w)
                 g_tag = edmonds.mst(('root', 'root', 0), weighted_full_graph)
-                if True:  # TODO: NEED HERE TO CHECK IF NOT EQUAL?  + GET OUT AUX
+                if True:  # TODO: NEED HERE TO CHECK IF NOT EQUAL?
                     w = w + self.get_saved_f_vector(data[0], data[4]) - self.get_f_vector(g_tag)
             print('Done ' + str(i+1) + ' iteration at ' + str(datetime.now()-iteration_time))
             self.save_w(w, i+1)
